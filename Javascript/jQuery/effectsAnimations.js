5e6cc95e8dd574c257490169d06913bc39709364
Ashish Gupta <ashishgupta1350@gmail.com>
2018-08-22 15:14:38 +0530

    Backend Development Added

---
+++ Javascript/jQuery/effectsAnimations.js
@@ -0,0 +1,6 @@
+/* <script type="text/javascript" src="jquery.js">
+</script> */
+$("div").fadeOut(400,function()
+    {
+        console.log("this works");
+    })
---
+++ Node Js/DemoApp/app.js
@@ -0,0 +1,9 @@
+
+var something=require("cat-me");
+console.log(something());
+
+var knockknock = require('knock-knock-jokes');
+ 
+console.log(knockknock()); // returns a knock knock joke
+
+console.log("FROM APP.JS");
